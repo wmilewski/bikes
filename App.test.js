128919cb2efd35f5bc99dbb7889fb5d813cbe414
wojtek <mail@wmilewski.com>
2017-12-29 20:12:13 +0000

    Parse API response

--- App.test.js
+++ App.test.js
@@ -3,7 +3,45 @@ import App from './App';
 
 import renderer from 'react-test-renderer';
 
+import {parseResponse} from './src/functions';
+
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
+});
+
+it('parses response', () => {
+    const response = require('./tests/response.json');
+    expect(parseResponse(response).slice(0, 3)).toEqual([
+        {
+            name: 'Warwick Avenue Station, Maida Vale',
+            distance: 1.11,
+            installed: true,
+            locked: false,
+            bikes: 18,
+            spaces: 1,
+            lat: 51.523344,
+            lon: -0.183846,
+        },
+        {
+            name: 'Bevington Road West, North Kensington',
+            distance: 1.2,
+            installed: true,
+            locked: false,
+            bikes: 14,
+            spaces: 9,
+            lat: 51.5212,
+            lon: -0.208888,
+        },
+        {
+            name: "All Saints' Road, Portobello",
+            distance: 1.23,
+            installed: true,
+            locked: false,
+            bikes: 21,
+            spaces: 1,
+            lat: 51.519042,
+            lon: -0.204764,
+        }
+    ]);
 });
---
+++ src/functions.js
@@ -0,0 +1,30 @@
+const parseAdditionalProperties = properties => properties.reduce((result, property) => {
+    switch (property.key) {
+        case 'Installed':
+            result.installed = property.value === 'true';
+            break;
+        case 'Locked':
+            result.locked = property.value === 'true';
+            break;
+        case 'NbBikes':
+            result.bikes = parseInt(property.value);
+            break;
+        case 'NbEmptyDocks':
+            result.spaces = parseInt(property.value);
+            break;
+    }
+
+    return result;
+}, {});
+
+const parsePlace = place => ({
+    name: place.commonName,
+    distance: Number((parseFloat(place.distance) / 1000).toFixed(2)),
+    lat: place.lat,
+    lon: place.lon,
+    ...parseAdditionalProperties(place.additionalProperties),
+});
+
+const parseResponse = response => response.places.map(parsePlace);
+
+export {parseResponse};
